f791297a8c4a60e840add2a42b87b6c7ff60d5d0
Sergey Kotovich <seirosso1@gmail.com>
2024-08-01 00:05:16 +0300

    First test!!!

--- src/test/java/SelenideSearchRepository.java
+++ src/test/java/SelenideSearchRepository.java
@@ -4,15 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSearchRepository {
 
 
     @BeforeAll
     public static void before() {
-        System.setProperty("webdriver.chrome.driver", "C:\\.cache\\selenium\\chromedriver\\win64\\126.0.6478.126\\chromedriver.exe");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
@@ -20,8 +18,14 @@ public class SelenideSearchRepository {
 
     @Test
     public void checkVisibleSelenideTest() {
-        open("https://github-landing.parthmadhvani2.vercel.app/");
-        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        open("https://github.com");
+        $("[data-target='qbsearch-input.inputButtonText']").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $$("[data-testid='results-list'] div").get(0).$("div:last-child a").click();
+        $$("#repository-container-header ul li").get(8).$("a").click();
+        $$("#wiki-body ul li").get(8).$("a").shouldHave(text("Soft assertions"));
+        $$("#wiki-body ul li").get(8).$("a").click();
+        $$(".markdown-body div.markdown-heading").get(5).shouldHave(text("3. Using JUnit5 extend test class:"));
 
     }
 }
